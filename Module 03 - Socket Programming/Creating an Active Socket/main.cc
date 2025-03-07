d80857414e8fc73966588b89bd2716f29baf1f21
imariom <marioallfred@gmail.com>
2025-03-07 19:25:36 +0200

    fixed creating active socket example

--- Module 03 - Socket Programming/Creating an Active Socket/main.cc
+++ Module 03 - Socket Programming/Creating an Active Socket/main.cc
@@ -2,39 +2,49 @@
 
 #include <boost/asio.hpp>
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    // Get server IP address and port number from the command line arguments
-    if (argc < 3)
+    // Check command line arguments.
+    if (argc != 3)
     {
-        std::cerr << "Usage: client <server-ip-address> <port-number>\n";
-        return -1;
+        std::cerr <<
+            "Usage: client <server-ip-address> <port-number>\n" <<
+            "Example:\n" <<
+            "   client 182.242.224.3 43280\n";
+        return EXIT_FAILURE;
     }
-    std::string serverIPAddress = argv[1];
-    std::size_t portNumber = std::atoi(argv[2]);
 
-    // Create the I/O execution context
+    // Create the I/O execution context.
     boost::asio::io_context ioCtx;
+
+    // Create an active socket (client)
+    boost::asio::ip::tcp::socket socket(ioCtx);
 
     try
     {
-        // Create a client endpoint provided the IP and port number
-        boost::asio::ip::tcp::endpoint endpoint(
-            boost::asio::ip::make_address(serverIPAddress), portNumber);
+        // Create the server IP address.
+        boost::asio::ip::address serverIPAddress =
+            boost::asio::ip::make_address(argv[1]);
 
-        // Create an active socket (client)
-        boost::asio::ip::tcp::socket socket(ioCtx);
+        // Get the server port number.
+        std::size_t portNumber = std::atoi(argv[2]);
+        
+        // Create the server TCP endpoint.
+        boost::asio::ip::tcp::endpoint serverEndpoint(
+            serverIPAddress, portNumber);
 
-        // Connect to the server (blocking-operation)
-        socket.connect(endpoint);
-        std::cout << "Connected to server at " << endpoint << std::endl;
+        // Connect to the server (blocking-operation).
+        socket.connect(serverEndpoint);
+
+        std::cout << "Connected to server: " << serverEndpoint << std::endl;
 
         // ... socket can now be used for I/O operations
     }
-    catch (std::exception& e)
+    catch (const std::exception& e)
     {
         std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
