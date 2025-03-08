4302d00b8d64925fe855acf3df74eca94a742327
imariom <marioallfred@gmail.com>
2025-03-08 10:39:42 +0200

    fixed binding a socket example

--- Module 03 - Socket Programming/Binding a Socket to an Endpoint/main.cc
+++ Module 03 - Socket Programming/Binding a Socket to an Endpoint/main.cc
@@ -1,38 +1,58 @@
 #include <iostream>
-#include <string>
 
 #include <boost/asio.hpp>
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    // Get server port number from the command line arguments
-    if (argc < 2)
+    // Check command line arguments.
+    if (argc != 2)
     {
-        std::cerr << "Usage: server <port-number>\n";
-        return -1;
+        std::cerr <<
+            "Usage: server <port-number>\n" <<
+            "Example:\n" <<
+            "   server 54321\n";
+        return EXIT_FAILURE;
     }
-    std::size_t portNumber = std::atoi(argv[1]);
     
     // Create the I/O execution context
     boost::asio::io_context ioCtx;
 
-    // Create a passive socket
-    boost::asio::ip::tcp::acceptor acceptor(ioCtx);
+    // Create the endpoint
+    boost::asio::ip::tcp::endpoint endpoint(
+        boost::asio::ip::tcp::v4(), std::atoi(argv[1]));
 
-    // Bind the acceptor to an endpoint
+    // Create a passive socket
     boost::system::error_code ec;
-    acceptor.bind(boost::asio::ip::tcp::endpoint(
-        boost::asio::ip::tcp::v4(), portNumber), ec);
+
+    boost::asio::ip::tcp::acceptor acceptor(ioCtx);
+    
+    // Open the socket
+    acceptor.open(endpoint.protocol(), ec);
 
     if (ec) {
-        // Failed to bind the acceptor socket.
-        std::cerr << "Error: " << ec.what() << ". Message"
-            << ec.message() << "\n";
+        // Failed to open the acceptor socket.
+        std::cerr
+            << "Error: " << ec.what()
+            << ". Message: " << ec.message() << "\n";
 
         return ec.value();
     }
 
+    // Bind the acceptor to an endpoint
+    acceptor.bind(endpoint, ec);
+
+    if (ec) {
+        // Failed to bind the acceptor socket.
+        std::cerr
+            << "Error: " << ec.what()
+            << ". Message: " << ec.message() << "\n";
+
+        return ec.value();
+    }
+
+    std::cout << "Socket bound to: " << endpoint << '\n';
+
     // ... then listen, accept and do I/O
     
-    return 0;
+    return EXIT_SUCCESS;
 }
